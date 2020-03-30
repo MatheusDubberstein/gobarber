e3012cb6f406e802367818783064f4570a9ef1a9
matheusDubberstein <matheusdub.s@gmail.com>
2020-03-30 13:49:00 -0300

    add: Schedule

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 import SessionsControler from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import multerConfig from './config/multer';
@@ -19,6 +20,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
